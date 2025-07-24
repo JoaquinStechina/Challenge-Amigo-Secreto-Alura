d9e1f01f389e281b6e63d7de158947886a5e9956
JoaquinStechina <joaquinstechina12@gmail.com>
2025-07-24 02:17:33 -0300

    feat: initial commit and finished whole project

---
+++ app.js
@@ -0,0 +1,39 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+const NOMBRE_AMIGO = document.getElementById("amigo");
+const LISTA = document.getElementById("listaAmigos");
+const RESULTADO = document.getElementById("resultado");
+
+function agregarAmigo() {
+  if (NOMBRE_AMIGO.value == "") {
+    alert("Por favor, inserte un nombre.");
+  } else {
+    amigos.push(NOMBRE_AMIGO.value);
+
+    NOMBRE_AMIGO.value = "";
+    LISTA.innerHTML = "";
+    RESULTADO.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+      const li = document.createElement("li");
+      li.textContent = amigos[i];
+      LISTA.appendChild(li);
+    }
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length > 0) {
+    let indice = Math.floor(Math.random() * amigos.length);
+    let amigo = amigos[indice];
+
+    LISTA.innerHTML = "";
+
+    const li = document.createElement("li");
+    li.textContent = `El amigo secreto es: ${amigo}`;
+    RESULTADO.appendChild(li);
+  } else {
+    alert("No hay amigos cargados en la lista!");
+  }
+}
